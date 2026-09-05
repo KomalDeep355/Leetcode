d75e81fd01d17b8d51668dc1d6c7c7413ee03727
KomalDeep355 <deepkomal355@gmail.com>
2026-09-05 18:02:56 +0530

    Time: 5 ms (49.1%), Space: 20.7 MB (5.14%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        count = 0
+        for i in nums:
+            if i == 0:
+                count +=1
+        non_zero = [num for num in nums if num != 0]
+        nums[:] = non_zero + [0] * count
+        return nums
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
