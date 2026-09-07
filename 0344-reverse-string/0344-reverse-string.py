274a011bae511e03224090101c200aae0b9886a6
KomalDeep355 <deepkomal355@gmail.com>
2026-09-07 07:55:57 +0530

    Time: 3 ms (45.2%), Space: 23.4 MB (81%) - LeetHub - Time: O(n), Space: O(1)

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,8 +3,12 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
-        return s
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
