b68f5e5d7b546b7cf8a860603a4d8d31b4c3a427
KomalDeep355 <deepkomal355@gmail.com>
2026-09-07 20:57:25 +0530

    Time: 19 ms (59.88%), Space: 19.2 MB (97.67%) - LeetHub - Time: O(len(s2)), Space: O(1)

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        left = 0
+        right = 0
+        count_s1 = {}
+        count_window = {}
+
+        for char in s1:
+            count_s1[char] = count_s1.get(char, 0) + 1
+
+        while right < len(s2):
+            count_window[s2[right]] = count_window.get(s2[right], 0) + 1
+
+            if right - left + 1 > len(s1):
+                count_window[s2[left]] -= 1
+
+                if count_window[s2[left]] == 0:
+                    del count_window[s2[left]]
+
+                left += 1
+
+            if count_window == count_s1:
+                return True
+
+            right += 1
+
+        return False
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
