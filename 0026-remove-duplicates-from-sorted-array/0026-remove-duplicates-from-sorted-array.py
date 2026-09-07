fdba4f770c7b2b6707a806c1826644f8f46448c8
KomalDeep355 <deepkomal355@gmail.com>
2026-09-07 07:47:40 +0530

    Time: 0 ms (100%), Space: 20.5 MB (42.86%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n == 0:
+            return 0
+
+        left = 1
+        k = 1
+
+        while left < n:
+            if nums[left] != nums[left - 1]:
+                nums[k] = nums[left]
+                k += 1
+
+            left += 1
+
+        return k
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
