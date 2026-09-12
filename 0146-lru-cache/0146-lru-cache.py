93cf24a450e908772d303e4e1dda09854b1c1a49
KomalDeep355 <deepkomal355@gmail.com>
2026-09-12 13:26:53 +0530

    Time: 199 ms (6.07%), Space: 29 MB (96.36%) - LeetHub - Time: O(capacity), Space: O(capacity)

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,37 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.order = []
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.order.remove(key)
+        self.order.append(key)
+
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache[key] = value
+            self.order.remove(key)
+            self.order.append(key)
+        else:
+            if len(self.cache) == self.capacity:
+                lru = self.order.pop(0)
+                del self.cache[lru]
+
+            self.cache[key] = value
+            self.order.append(key)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
