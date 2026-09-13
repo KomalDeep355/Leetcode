a1c69aa2507f43e87b7d43807f3bed3b291107ce
KomalDeep355 <deepkomal355@gmail.com>
2026-09-13 21:38:46 +0530

    Time: 8 ms (41.29%), Space: 21.1 MB (84.06%) - LeetHub - Time: O(n), Space: O(n)

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -6,3 +6,7 @@ class Solution:
              if counts[num] > len(nums) // 2:
                 return num
                  
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
