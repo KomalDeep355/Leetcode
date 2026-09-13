2fb356892a9959a1842c3ac51ca649b321bd2311
KomalDeep355 <deepkomal355@gmail.com>
2026-09-13 21:37:15 +0530

    Time: 33 ms (53.71%), Space: 21.8 MB (84.68%) - LeetHub - Time: O(n), Space: O(n)

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -9,3 +9,7 @@ class Solution:
             count += freq.get(sum - k, 0)
             freq[sum] = freq.get(sum, 0) + 1
         return count
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
